00d2e02226405b7740c39b0687c103c61ba9f315
nngocjade <nngocjade@gmail.com>
2020-04-06 19:07:26 +0800

    in progress

--- assets/js/index.js
+++ assets/js/index.js
@@ -18,22 +18,59 @@ let chartData = {
   series: [],
 };
 
+let charOptions = {
+  high: 0,
+  low: 0,
+  filWidth: true,
+};
+
+let variableData = {
+  deathSeries: [],
+  hospitalizedSeries: [],
+  totalCasesSeries: [],
+};
+
+let myChart;
+
 fetch("https://covidtracking.com/api/us/daily")
   .then((res) => res.json())
-  .then((dataObj) => {
-    let seriesArr = [];
-    console.log(dataObj);
-    for (let index = 10; index > 0; index--) {
-      const day = dataObj[index];
+  .then((dataArr) => {
+    for (let index = 10; index >= 0; index--) {
+      console.log(dataArr);
+      const daydata = data[index];
+      console.log(daydata);
       let dateStr = day.date.toString();
       chartData.labels.push(
         `${dateStr.substr(4, 2)}/${dateStr.substr(6, 2)}/${dateStr.substr(
           0,
-          4
+          2
         )}}`
       );
-      seriesArr.push(day.death);
+      //add data point to series
+      variableData.deathSeries.push(dayData.death);
+      //set highest y axis point
+      if (daydata.death > chartOptions.high) {
+        chartOptions.high = dayData.death;
+      }
+      //add data point to series
+      variableData.hospitalizedSeries.push(dayData.hospitalized);
+      //set highest y axis point
+      if (dayData.hospitalized > chartOptions.high) {
+        chartOptions.high = dayData.hospitalized;
+      }
+      //add data point to series
+      variableData.totalCasesSeries.push(dayData.total);
+      //set highest y axis point
+      if (dayData.total > chartOptions.high) {
+        chartOptions.high = dayData.total;
+      }
     }
-    chartData.series.push(seriesArr);
-    new Chartist.Line(".ct-chart", chartData);
-  });
+    chartData.series[0] = variableData.totalCasesSeries;
+    myChart = new Chartist.Line(".ct-chart", chartData, chartOptions);
+    creatButton();
+  })
+
+
+function creatButton(){
+    let buttons = 
+}
